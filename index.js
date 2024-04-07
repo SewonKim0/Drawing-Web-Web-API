1f0e49b2570629f89db1f2a1b349ab09c58a8f9e
Sewon Kim <sewonkim0black@gmail.com>
2024-04-07 19:26:25 -0400

    sewon - setup code for img generation, created right canvas initialization with sample dummy img

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
 const left = document.getElementById('left');
 const right = document.getElementById('right');
 const ctx = right.getContext("2d");
+
+// get api key
+let key = null;
+fetch(".env")
+    .then(response => response.text())
+    .then(text => {
+        key = text;
+    });
+
+// button click: get dalle image
+document.getElementById("generate").addEventListener("click", () => {
+    if (key === null) {
+        console.log("Error: API key not found");
+        return;
+    }
+
+    // get img prompt
+    const prompt = document.getElementById("input").value;
+
+    // TEST (use dummy img instead of generated img)
+    applyTransformations("./dummy.png");
+
+    // // get dalle img
+    // fetch("https://api.openai.com/v1/images/generations", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Authorization": `Bearer ${key}`
+    //     },
+    //     body: JSON.stringify({
+    //         prompt: prompt,
+    //         n: 1
+    //     })
+    // })
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         const url = json.data[0].url
+    //         applyTransformations(url);
+    //     })
+});
+
+/**
+ * This function applys graphical transformations to the right canvas based on given url image
+ * @param {String} url String url to img
+ */
+function applyTransformations(url) {
+    const img = new Image();
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, right.width, right.height);
+    };
+    img.src = url;
+}
