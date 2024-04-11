4a3d4805c43f3008c6c7ddd43fb2b3cbf6c26756
Claire <116824003+clairekim6872@users.noreply.github.com>
2024-04-10 21:23:33 -0400

    Updating index.js with canvas drawings

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const left = document.getElementById('left');
 const right = document.getElementById('right');
-const ctx = right.getContext("2d");
+const context = right.getContext("2d");
+const lcontext = left.getContext("2d");
+
+let width;
+let height;
+let lwidth;
+let lheight;
+
+// set the number of canvas, scaled for screen resolution
+let pxScale = window.devicePixelRatio;
 
 // get api key
 let key = null;
@@ -48,8 +57,106 @@ document.getElementById("generate").addEventListener("click", () => {
  */
 function applyTransformations(url) {
     const img = new Image();
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0, right.width, right.height);
+
+    img.onload = function () {
+        // context.drawImage(img, 0, 0, right.width, right.height);
+        getColors(img)
     };
     img.src = url;
 }
+
+function setup() {
+    // set up dimensions for left canvas drawing
+    lwidth = left.getBoundingClientRect().width;
+    lheight = left.getBoundingClientRect().height;
+
+    left.width = lwidth * pxScale;
+    left.height = lheight * pxScale;
+
+    // set up dimensions for right canvas drawing
+    width = right.getBoundingClientRect().width;
+    height = right.getBoundingClientRect().height;
+
+    right.width = width * pxScale;
+    right.height = height * pxScale;
+
+    // normalize the coordinate system
+    lcontext.scale(pxScale, pxScale);
+    context.scale(pxScale, pxScale);
+
+    // create a grid of squares inside left canvas
+    const scale = 40;
+
+    for (let i = 0; i < left.width; i++) {
+        for (let j = 0; j < left.height; j++) {
+            // draw grid of circles
+            lcontext.save();
+            lcontext.beginPath();
+            lcontext.rect(i * scale / pxScale, j * scale / pxScale, 19, 19);
+            lcontext.fill();
+            lcontext.restore();
+        }
+    }
+}
+
+// array of pixel colors
+const pixelColors = [];
+
+function getColors(image) {
+    context.drawImage(image, 0, 0, 45, 30);
+
+    // sampling image data
+    let imageData = context.getImageData(0, 0, right.width / 40, right.height / 40);
+    let data = imageData.data;
+
+    context.clearRect(0, 0, width, height);
+
+    for (let channel = 0; channel < data.length; channel += 4) {
+        let color = `rgba(${data[channel]}, ${data[channel + 1]}, ${data[channel + 2]}, ${data[channel + 3]})`;
+        pixelColors.push(color);
+    }
+
+    // create a grid of squares inside right canvas
+    const scale = 40;
+
+    for (let i = 0; i < right.width; i++) {
+        for (let j = 0; j < right.height; j++) {
+            // get random color
+            let color = pixelColors[Math.floor(Math.random() * pixelColors.length)];
+
+            // recolor grid of circles
+            context.save();
+            context.beginPath();
+            context.rect(i * scale / pxScale, j * scale / pxScale, 20, 20);
+            context.fillStyle = color;
+            context.fill();
+            context.restore();
+        }
+    }
+}
+
+function draw() {
+    // create a grid of squares inside right canvas
+    const scale = 40;
+
+    for (let i = 0; i < right.width; i++) {
+        for (let j = 0; j < right.height; j++) {
+            context.save();
+            context.beginPath();
+            context.rect(i * scale / pxScale, j * scale / pxScale, 19, 19);
+            context.fill();
+            context.restore();
+        }
+    }
+}
+
+// wait for the DOM to load, including dependent resources
+window.addEventListener('load', () => {
+    setup();
+    draw();
+});
+
+window.addEventListener('resize', () => {
+    setup();
+    draw();
+});
